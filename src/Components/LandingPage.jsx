870454f897679bc4ad63892d55bd954a702afc2a
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-28 07:43:01 +0100

    actively

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,12 +4,16 @@ import Navigation from './Nav/Navigation'
 const LandingPage = () => {
   return (
 
-              <div className='bg-black'>
-          <Navigation />
-        <div className='w-[346px] mb-[64px] flex justify-end'>
-            <div className='w-full h-[346px]  bg-white'>
+        <div className='bg-black'>
+
+        <div>
+         <Navigation />
         </div>
-     </div>
+      
+        <div className='w-[346px] border border-red-300 mb-[64px] flex justify-end'>
+            <div className='w-full h-[346px]  bg-white'></div>
+        </div>
+
     </div>
   )
 }

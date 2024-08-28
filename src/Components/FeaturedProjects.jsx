8e334e247999a6be9f357c0a9168e38d2bb3715a
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-28 09:37:39 +0100

    pages

--- src/Components/FeaturedProjects.jsx
+++ src/Components/FeaturedProjects.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const FeaturedProjects = () => {
   return (
-    <div className='mx-24 font-interTight'>
+    <div className='mx-24 font-interTight py-12 bg-white'>
         <div>
               <h2 className='text-3xl py-6'>Featured Projects</h2>  
         </div>
 
 
-        <div className='grid grid-cols-3 gap-4 '>
+        <div className='grid grid-cols-3 gap-2'>
                <div className='bg-slate-300 h-[450px] rounded'></div>
                <div className='bg-slate-300 h-[450px] rounded '></div>
                <div className='bg-slate-300 h-[450px] rounded '></div>
---
+++ src/Components/Pages/Thoughts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Thoughts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Thoughts

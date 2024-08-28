b3425c6b15543ad4714617424f0e80b97bb8c2ed
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-28 09:39:03 +0100

    works

--- src/Components/Pages/Works.jsx
+++ src/Components/Pages/Works.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>
-      
+    <div className='mx-24 font-interTight'>
+        <div>
+              <h2 className='text-3xl py-6'>All Works</h2>  
+        </div>
+
+
+        <div className='grid grid-cols-2 gap-4 '>
+               <div className='bg-slate-300 h-[400px] rounded'></div>
+               <div className='bg-slate-300 h-[350px] rounded '></div>
+               <div className='bg-slate-300 h-[450px] rounded '></div>
+        </div>
+
+
+        <div className='flex justify-end py-4 text-xl'>
+            View All
+        </div>
+     
     </div>
   )
 }

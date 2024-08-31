6cfb062626bff8d399585d71be3d3440d33ca00c
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-31 10:53:02 +0100

    flo

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='bg-black text-white py-32 px-24 font-interTight'>
 
       <div>
-        Let's talk
+       <h1 className='text-[64px]'>Let's talk</h1> 
       </div>
 
 
-      <div>
+      <div className='w-[50%] '>
 
-        <p>
+        <p className='py-8'>
         Whether it's a conversation about all things design or 
         thinking about a potential project or opportunity, get in touch.
         </p>
 
-        <div>
-          <div>
-              <input type="text" name="" placeholder='Name' id="" />
-              <input type="text" name="" placeholder='Email' id="" />
+        <div className='flex flex-col gap-4' >
+          <div className='flex gap-4'>
+              <input type="text" name="" placeholder='Name' id="" className='bg-[#2e2e2e] w-[50%] py-2 px-1 text-white' />
+              <input type="text" name="" placeholder='Email' id=""  className='bg-[#2e2e2e] px-1 w-[50%] text-white' />
           </div>
 
           <div>
-              <input type="text" name="" placeholder='Message' id="" />
+              <input type="text" name="" placeholder='Message' id=""  className='bg-[#2e2e2e] px-1 pb-24  w-full text-white' />
           </div>
 
         </div>
@@ -32,7 +32,7 @@ const Contact = () => {
       </div>
 
       <div>
-      <button>Send</button>
+      <button className='w-[50%] mt-4 py-4 px-[auto] bg-white text-black'>Send</button>
       </div>
 
 

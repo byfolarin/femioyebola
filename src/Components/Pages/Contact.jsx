c2f5a850187cfa32e6ad8ad017380cd4a081149f
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-31 10:26:43 +0100

    flex

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -3,7 +3,30 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      
+
+      <div>
+        Let's talk
+      </div>
+
+
+      <div>
+        <p>
+        Whether it's a conversation about all things design or 
+        thinking about a potential project or opportunity, get in touch.
+        </p>
+
+        <div>
+          <div>
+              <input type="text" name="" id="" />
+              <input type="text" name="" id="" />
+          </div>
+
+          <input type="text" name="" id="" />
+
+        </div>
+      </div>
+
+
     </div>
   )
 }

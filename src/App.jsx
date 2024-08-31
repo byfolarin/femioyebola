36999884b1315eb6bc91e1793ec318bcd76b9127
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-31 09:26:49 +0100

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import './App.css'
-import Navigation2 from './Components/Nav/Navigation2'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Navigation2 from './Components/Nav/Navigation2'
 import LandingPage from './Components/LandingPage'
 import FeaturedProjects from './Components/FeaturedProjects'
 import Section2 from './Components/Section2'
 import BlogSec from './Components/BlogSec'
 import Footer from './Components/Footer'
+import Works from './Components/Pages/Works'
+import About from './Components/Pages/About'
+import Contact from './Components/Pages/Contact'
+import Thoughts from './Components/Pages/Thoughts'
 
 function App() {
-
-
   return (
-    <>
-    {/* <div className='fixed z-50'>
-       <Navigation2 />
-      
-     </div> */}
-      <LandingPage />
-     <FeaturedProjects />
-     <Section2 />
-     <BlogSec />
-     <Footer />
-    </>
+    <Router>
+      {/* <div className='fixed z-50'>
+        <Navigation2 />
+      </div> */}
+      <Routes>
+        <Route path="/" element={
+          <>
+            <LandingPage />
+            <FeaturedProjects />
+            <Section2 />
+            <BlogSec />
+          </>
+        } />
+        <Route path="/works" element={<Works />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/thoughts" element={<Thoughts />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
--- src/Components/Nav/Navigation.jsx
+++ src/Components/Nav/Navigation.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
-    <div className='font-interTight w-full flex items-end justify-between pr-[248px] pt-6 py-4 px-24 text-white' >
-      
-
-      <div><h2 className='font-bold text-3xl'>femiOyebola.</h2></div>
-
-      <div className='w-[348px]'>
-
-            <div className='flex gap-11'>
-                <div>About</div>
-                <div>Projects</div>
-                <div>Contact</div>
-                <div>Thoughts</div>
-            </div>
-
+    <div className='font-interTight w-full flex items-end justify-between pr-[248px] pt-6 py-4 px-24 text-white'>
+      <div>
+        <Link to="/" className='font-bold text-3xl'>femiOyebola.</Link>
       </div>
 
+      <div className='w-[348px]'>
+        <div className='flex gap-11'>
+          <Link to="/about">About</Link>
+          <Link to="/works">Projects</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/thoughts">Thoughts</Link>
+        </div>
+      </div>
     </div>
   )
 }

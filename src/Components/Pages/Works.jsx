bee637e7ad3d8311ae187c628969855358303991
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-01 01:48:35 +0100

    hoverlay

--- src/Components/Pages/Works.jsx
+++ src/Components/Pages/Works.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
-import { motion, useScroll, useTransform } from 'framer-motion'
 import Section from './Section'
 import House1 from '../../../src/assets/images/House1.jpeg'
 import House2 from '../../../src/assets/images/House2.jpeg'
 import House3 from '../../../src/assets/images/House3.jpeg'
 import House4 from '../../../src/assets/images/House4.jpeg'
 import House5 from '../../../src/assets/images/House5.jpeg'
-
-const ParallaxImage = ({ src, alt }) => {
-  const { scrollYProgress } = useScroll()
-  const y = useTransform(scrollYProgress, [0, 1], ['0%', '30%'])
-
-  return (
-    <motion.div className='h-full overflow-hidden'>
-      <motion.img 
-        src={src} 
-        alt={alt} 
-        className='w-full h-full object-cover'
-        style={{ y }}
-      />
-    </motion.div>
-  )
-}
 
 const Works = () => {
   return (
@@ -32,25 +15,11 @@ const Works = () => {
         </div>
 
         <div className='grid grid-cols-2 gap-4'>
-          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
-            <ParallaxImage src={House1} alt="House 1" />
-          </div>
-
-          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
-            <ParallaxImage src={House2} alt="House 2" />
-          </div>
-
-          <div className='bg-slate-300 h-[400px] col-span-2 rounded overflow-hidden'>
-            <ParallaxImage src={House3} alt="House 3" />
-          </div>
-
-          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
-            <ParallaxImage src={House4} alt="House 4" />
-          </div>
-
-          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
-            <ParallaxImage src={House5} alt="House 5" />
-          </div>
+          <ImageWithOverlay src={House1} projectName="Project 1" />
+          <ImageWithOverlay src={House2} projectName="Project 2" />
+          <ImageWithOverlay src={House3} projectName="Project 3" className="col-span-2" />
+          <ImageWithOverlay src={House4} projectName="Project 4" />
+          <ImageWithOverlay src={House5} projectName="Project 5" />
         </div>
 
         <div className='flex justify-end py-4 text-xl'>
@@ -60,5 +29,15 @@ const Works = () => {
     </div>
   )
 }
+
+const ImageWithOverlay = ({ src, projectName, className = '' }) => (
+  <div className={`bg-slate-300 h-[400px] rounded overflow-hidden relative group ${className}`}>
+    <img src={src} alt={projectName} className='w-full h-full object-cover' />
+    <div className='absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300'></div>
+    <div className='absolute bottom-4 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+      {projectName}
+    </div>
+  </div>
+)
 
 export default Works

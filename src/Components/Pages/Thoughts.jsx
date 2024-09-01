945f72c7b9c387e0104d553654d178e09274d9dc
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-01 01:53:54 +0100

    dark-overlay

--- src/Components/Pages/Thoughts.jsx
+++ src/Components/Pages/Thoughts.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 const Thoughts = () => {
   return (
-    <div>
+    <div className='mt-24 font-interTight mx-16'>
+
+
+      <div className='w-[100%] flex justify-end py-12'>
+          <h1 className='text-5xl w-[60%] '>
+            My thoughts on design, 
+            technology and events.
+          </h1>
+      </div>
+
+      <div>
       
+            <div className='grid grid-cols-3 gap-4 py-6 '>
+                    <div className='bg-slate-300 h-[400px] rounded'></div>
+                    <div className='bg-slate-300 h-[350px] rounded '></div>
+                    <div className='bg-slate-300 h-[450px] rounded '></div>
+              </div>
+
+
+              <div className='grid grid-cols-3 gap-4 py-6 '>
+                    <div className='bg-slate-300 h-[400px] rounded'></div>
+                    <div className='bg-slate-300 h-[350px] rounded '></div>
+                    <div className='bg-slate-300 h-[450px] rounded '></div>
+              </div>
+
+        </div>
+
+
+
     </div>
   )
 }

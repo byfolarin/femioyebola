01e163268d1d72d35b821ea7858d4e3e8db40d8c
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-01 01:36:35 +0100

    col-span-2

--- src/Components/Pages/Works.jsx
+++ src/Components/Pages/Works.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Section from './Section'
+import House1 from '../../../src/assets/images/House1.jpeg'
 
 const Works = () => {
   return (
@@ -9,11 +10,30 @@ const Works = () => {
         </div>
 
         <div className='grid grid-cols-2 gap-4 '>
-          <div className='bg-slate-300 h-[400px] rounded'></div>
-          <div className='bg-slate-300 h-[400px] rounded '></div>
-          <div className='bg-slate-300 col-span-2 h-[450px] rounded '></div>
-          <div className='bg-slate-300 h-[400px] rounded '></div>
-          <div className='bg-slate-300 h-[400px] rounded '></div>
+          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
+              <img src={House1} alt="" className='w-full h-full object-cover'/>
+            </div>
+
+
+           <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
+              <img src={House1} alt="" className='w-full h-full object-cover'/>
+            </div>
+
+
+          <div className='bg-slate-300 h-[400px] col-span-2 rounded overflow-hidden'>
+              <img src={House1} alt="" className='w-full h-full object-cover'/>
+            </div>
+
+
+          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
+              <img src={House1} alt="" className='w-full h-full object-cover'/>
+            </div>
+
+
+          <div className='bg-slate-300 h-[400px] rounded overflow-hidden'>
+              <img src={House1} alt="" className='w-full h-full object-cover'/>
+            </div>
+            
         </div>
 
         <div className='flex justify-end py-4 text-xl'>

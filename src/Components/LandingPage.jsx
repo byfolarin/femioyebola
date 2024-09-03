e356fcc2fcd6f943f93fb513e21a3b7188dd6740
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-03 11:49:59 +0100

    h1

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -16,12 +16,12 @@ const LandingPage = () => {
                 <div className='w-[366px] py-[16px]'>
                     <div className='w-full h-[266px] rounded flex justify-end bg-[#DCE0E6]'></div>
                 </div>
-
+{/* 
                 <div>
                     <h2 className='font-interTight text-3xl text-white'>
                         Architect from Lagos, Nigeria.
                     </h2>
-                </div>
+                </div> */}
 
                 <div>
                     <p className='text-gray-400 pt-2'>Elevate your brand in the modern world with my expertise in digital 

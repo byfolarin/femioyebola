87e68b335f5e7ffa1a9002a268523ab87790db31
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-12 00:54:00 +0100

    thoughts

--- src/Components/Pages/Thoughts.jsx
+++ src/Components/Pages/Thoughts.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 const Thoughts = () => {
   return (
-    <div className='mt-24 font-interTight mx-16'>
+    <div className='mt-24 font-interTight mx-2 lg:mx-16'>
 
 
       <div className='w-[100%] flex justify-end py-12'>
-          <h1 className='text-5xl w-[60%] '>
-            My thoughts on design, 
+          <h1 className='text-5xl lg:w-[60%] '>
+            My thoughts on Architecture, 
             technology and events.
           </h1>
       </div>
 
       <div>
       
-            <div className='grid grid-cols-3 gap-4 py-6 '>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 py-6 '>
                     <div className='bg-slate-300 h-[400px] rounded'></div>
                     <div className='bg-slate-300 h-[350px] rounded '></div>
                     <div className='bg-slate-300 h-[450px] rounded '></div>
               </div>
 
 
-              <div className='grid grid-cols-3 gap-4 py-6 '>
+              <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 py-6 '>
                     <div className='bg-slate-300 h-[400px] rounded'></div>
                     <div className='bg-slate-300 h-[350px] rounded '></div>
                     <div className='bg-slate-300 h-[450px] rounded '></div>

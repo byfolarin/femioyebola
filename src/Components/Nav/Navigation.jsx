90748644af5f15af60dcf52fa5d053205715801e
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-12 02:56:21 +0100

    hall

--- src/Components/Nav/Navigation.jsx
+++ src/Components/Nav/Navigation.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom'; // Added useLocation
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation(); // Added this line
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isOpen && !event.target.closest('.nav-container')) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    // Close menu on route change
+    setIsOpen(false);
+  }, [location]); // Added location as a dependency
+
   return (
-    <div className='font-interTight w-full flex items-center justify-between lg:pr-[248px] lg:pt-6 py-4 lg:py-4 px-2 lg:px-24 text-white bg-black sticky top-0 z-50'>
+    <div className='nav-container font-interTight w-full flex items-center justify-between lg:pr-[248px] lg:pt-6 py-4 lg:py-4 px-2 lg:px-24 text-white bg-black sticky top-0 z-50'>
       <div className='whitespace-nowrap'>
         <Link to="/" className='font-bold text-3xl text-nowrap'>Femi Oyebola</Link>
       </div>
 
-      <div className='lg:hidden flex justify-end'>
-        <button onClick={toggleMenu} className='text-white font-interTight flex text-base'>
-          {isOpen ? 'CLOSE' : 'MENU'}
+      <div className='lg:hidden'>
+        <button onClick={toggleMenu} className='text-white'>
+          {isOpen ? 'Close' : 'Menu'}
         </button>
       </div>
 
